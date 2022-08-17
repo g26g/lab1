e3a5c8b7fcfb5fb37d08624cd82df8fa0df81d93
g26g <111423414+g26g@users.noreply.github.com>
2022-08-17 18:00:28 +0800

    Add files via upload

---
+++ summer1/pages/index/index.js
@@ -0,0 +1,82 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src:'/images/image1.png',
+    name:'hello world'
+  },
+  getMyInfo: function(){
+    var that = this;
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log("获取用户信息成功", res);
+        that.setData({
+          src: res.userInfo.avatarUrl,
+          name: res.userInfo.nickName
+        }) 
+      },
+      fail: res => {
+        console.log("获取用户信息失败", res)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+})
